56581b757849b7105b8e0bfafe22ffe61f3ab21b
sharanyadabas <sd699@cornell.edu>
2023-08-04 18:25:21 -0400

    Fixed drag bug. Added Slider to change grid size. Added style scheme for entire website.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,24 @@
 const container = document.querySelector('.container')
-const gridSize = 10
-let colorMode = 'original'
-let drawMode = 'hover'
-let clicked = false;
+let gridSize = 5
+let colorMode = ''
+let drawMode = ''
+let clicked = false
+let grid
+
+const slider = document.querySelector('#myRange')
+const sliderText = document.querySelector('#sliderText')
+
+sliderText.textContent = `${slider.value} x ${slider.value}`
+slider.oninput = () => {
+    sliderText.textContent = `${slider.value} x ${slider.value}`
+    removeGrid()
+    rainbowBTN.classList.remove('selected')
+    originalBTN.classList.remove('selected')
+    hoverBTN.classList.remove('selected')
+    dragBTN.classList.remove('selected')
+    gridSize = slider.value
+    createGrid()
+}
 
 const originalBTN = document.querySelector('#original')
 const rainbowBTN = document.querySelector('#rainbow')
@@ -10,77 +26,101 @@ const dragBTN = document.querySelector('#drag')
 const hoverBTN = document.querySelector('#hover')
 
 originalBTN.onclick = () => {
+    rainbowBTN.classList.remove('selected')
+    originalBTN.classList.add('selected')
     colorMode = 'original'
     reset()
+    draw()
 }
 
 rainbowBTN.onclick = () => {
+    originalBTN.classList.remove('selected')
+    rainbowBTN.classList.add('selected')
     colorMode = 'rainbow'
     reset()
+    draw()
 }
 
 dragBTN.onclick = () => {
+    hoverBTN.classList.remove('selected')
+    dragBTN.classList.add('selected')
     drawMode = 'drag'
     reset()
+    draw()
 }
 
 hoverBTN.onclick = () => {
+    dragBTN.classList.remove('selected')
+    hoverBTN.classList.add('selected')
     drawMode = 'hover'
     reset()
+    draw()
 }
+
+createGrid()
+draw()
+
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        const parent = document.createElement('div')
+        parent.classList.add('row')
+        container.appendChild(parent)
+
+        for (let j = 0; j < gridSize; j++) {
+            const child = document.createElement('div')
+            child.classList.add('box')
+            parent.appendChild(child)
+        }
+    }
+}
+
+function removeGrid() {
+    const rows = document.querySelectorAll('.row')
+    rows.forEach((row) => container.removeChild(row))
+}
+
 function reset() {
-    console.log(`${colorMode} and ${drawMode}`)
+    clicked = false
     grid.forEach((pixel) => {
         pixel.classList.remove('colored', 'hover1', 'hover2', 'hover3', 'hover4', 'hover5')
         pixel.style = ""
     })
 }
 
-for (let i = 0; i < gridSize; i++) {
-    const arr = ['a', 'b', 'c', 'd']
-
-    const parent = document.createElement('div')
-    parent.setAttribute('id', arr[i])
-    parent.classList.add('row')
-    container.appendChild(parent)
-
-    for (let j = 0; j < gridSize; j++) {
-        const child = document.createElement('div')
-        child.classList.add('box')
-        parent.appendChild(child)
-    }
-}
-
-const grid = document.querySelectorAll('.box')
-
-if (drawMode === 'drag') {
+function draw() {
+    grid = document.querySelectorAll('.box')
     grid.forEach((pixel) => {
-        pixel.addEventListener('mousedown', () => clicked = true)
-        pixel.addEventListener('mousemove', dragged);
-        pixel.addEventListener('mouseup', () => clicked = false)
+        if (drawMode === 'drag') {
+            pixel.addEventListener('mousedown', () => clicked = true)
+            pixel.addEventListener('mousemove', dragged)
+            pixel.addEventListener('mouseup', () => clicked = false)
+        }
+        if (drawMode === 'hover')
+            pixel.addEventListener('mouseover', hover)
     })
 }
 
-if (drawMode === 'hover') {
-    grid.forEach((pixel) => pixel.addEventListener('mouseover', hover))
-}
-
 function hover() {
+    if (drawMode === 'drag')
+        return
     if (colorMode === 'original')
         colorOriginal(this)
-    else if (colorMode === 'rainbow')
+    if (colorMode === 'rainbow')
         colorRainbow(this)
 }
 
 function dragged() {
-    if (clicked && colorMode === 'original')
-        colorOriginal(this)
-    else if (clicked && colorMode === 'rainbow')
-        colorRainbow(this)
+    if (drawMode === 'hover')
+        return
+    if (clicked) {
+        if (colorMode === 'original')
+            colorOriginal(this)
+        else if (colorMode === 'rainbow')
+            colorRainbow(this)
+    }
 }
 
 function colorOriginal(obj) {
-    console.log("color")
     if (obj.classList.contains('hover1')) {
         obj.classList.remove('hover1')
         obj.classList.add('hover2')
